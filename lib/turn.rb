c7c54d400409ba49207f9b8f1cc0e9abfa5f1db8
Carrie Jones <c.jessica.jones@gmail.com>
2020-05-18 21:33:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,20 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+return user_input.to_i - 1
+end
 
-
+def move(array, index, value = "X")
+array[index] = value
+return array
+end
 
 # code your #valid_move? method here
 def valid_move?(board, index)

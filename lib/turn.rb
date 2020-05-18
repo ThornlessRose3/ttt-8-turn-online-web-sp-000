012806e1737c707614a0d2968669d2a01a6a2e6a
Carrie Jones <c.jessica.jones@gmail.com>
2020-05-18 21:39:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,4 +36,11 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  gets.strip(input)
+  input_to_index(input)
+  if valid_move?(board, index)
+    move(board,index)
+  else
+    turn(board)
+  end
 end
